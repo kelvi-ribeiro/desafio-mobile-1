bbf96703ffe331b3456d1e7d01a77ab74d4ab958
Kelvi Martins <kelvi.ribeiro@unigranrio.br>
2017-11-24 23:29:28 -0200

    Page agenda concluída

---
+++ ionic/src/pages/agenda/agenda-modal.ts
@@ -0,0 +1,51 @@
+import { AgendaService } from '../../providers/agenda-service';
+import { Component, ViewChild } from '@angular/core';
+import { NavParams, ViewController, ToastController, NavController } from 'ionic-angular';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  templateUrl: './agenda-modal.html'
+})
+export class AgendaModalPage {
+  @ViewChild('nome') nome;
+  
+  agenda: any = {};
+  error: any;
+  
+
+  constructor(public agendaService: AgendaService,              
+              public params: NavParams,
+              public viewCtrl: ViewController,
+              public toastCtrl: ToastController,
+              public navCtrl: NavController) {
+    if (this.params.data.id) {
+      this.agendaService.get(this.params.get('id')).subscribe(agenda => {
+        this.agenda = agenda;
+        this.agenda.href = agenda._links.self.href;
+        
+      });
+    }
+  }
+
+  dismiss() {
+    this.viewCtrl.dismiss();
+  }
+
+  save(form: NgForm) {
+    let update: boolean = form['href'];
+    this.agendaService.save(form).subscribe(result => {
+      let toast = this.toastCtrl.create({
+        message: 'Agenda "' + form.name + '" ' + ((update) ? 'updated' : 'added') + '.',
+        duration: 2000
+      });
+      toast.present();
+      this.dismiss();
+    }, error => this.error = error)
+  }
+
+  ionViewDidLoad() {
+    setTimeout(() => {
+      this.nome.setFocus();
+    },400);
+  }
+}
